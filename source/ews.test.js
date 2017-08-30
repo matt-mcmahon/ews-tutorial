d10ea47f7dc01805194b1876c292b5b8136d17a3
Matt McMahon <matt@broken-toys.com>
2017-08-30 17:17:48 -0400

    Refactored ews.test.js

--- source/ews.test.js
+++ source/ews.test.js
@@ -5,66 +5,70 @@ const test = require('blue-tape')
 const ews = require('./ews')
 
 test('ews.test.js', async t => {
-  {
-    const actual = typeof ews
-    const expected = 'object'
-    const message = 'ews should export an object'
-    t.same(actual, expected, message)
-  }
-
-  t.test('ews.create', async t => {
+  t.test('module properties', async t => {
     {
-      const actual = typeof ews.create
-      const expected = 'function'
-      const message = 'ews should have a create method'
+      const actual = typeof ews
+      const expected = 'object'
+      const message = 'ews should export an object'
       t.same(actual, expected, message)
     }
 
     {
-      const actual = ews.create().constructor
+      const actual = Object.keys(ews).sort()
+      const expected = ['create'].sort()
+      const message = `ews should have these properties: ${expected.join()}`
+      t.same(actual, expected, message)
+    }
+
+    {
+      const actual = typeof ews.create
+      const expected = 'function'
+      const message = 'ews.create should be a function'
+      t.same(actual, expected, message)
+    }
+  })
+
+  const serverPromise = ews.create()
+
+  t.test('ews.create', async t => {
+    {
+      const actual = serverPromise.constructor
       const expected = Promise
       const message = 'invoking ews.create should return a Promise'
       t.same(actual, expected, message)
     }
   })
 
-  const server = await ews.create()
+  const server = await serverPromise
 
-  t.test('ews.create -> server', async t => {
+  t.test('ews server instance', async t => {
     {
       const actual = typeof server
       const expected = 'object'
-      const message = 'created server is an object'
+      const message = 'server instance should be an object'
       t.same(actual, expected, message)
     }
 
     {
       const actual = server.constructor
       const expected = ews.create
-      const message = `server's constructor should be ews.create`
+      const message = `server.constructor should be ews.create`
       t.same(actual, expected, message)
     }
 
     {
       const actual = server.propertyIsEnumerable('constructor')
       const expected = false
-      const message = 'constructor property should NOT be enumerable'
+      const message = 'server.constructor should NOT be enumerable'
       t.same(actual, expected, message)
     }
   })
 
-  test('server.listen', async t => {
+  t.test('server.listen', async t => {
     {
       const actual = typeof server.listen
       const expected = 'function'
-      const message = 'server should have a listen method'
-      t.same(actual, expected, message)
-    }
-
-    {
-      const actual = server.listen().constructor
-      const expected = Promise
-      const message = 'invoking server.listen should return a Promise'
+      const message = 'server.listen should be a function'
       t.same(actual, expected, message)
     }
 
@@ -75,4 +79,17 @@ test('ews.test.js', async t => {
       t.same(actual, expected, message)
     }
   })
+
+  const listeningServerPromise = server.listen()
+
+  t.test(`server.listen()`, async t => {
+    {
+      const actual = listeningServerPromise.constructor
+      const expected = Promise
+      const message = 'invoking server.listen should return a Promise'
+      t.same(actual, expected, message)
+    }
+  })
+
+  // const listeningServer = await listeningServerPromise
 })
